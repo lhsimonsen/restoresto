3c0e7989722e52ef414228db3d7a74044fede422
Lars Simonsen <lars.simonsen@etraveli.com>
2018-10-30 17:49:46 +0100

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 /* eslint-disable class-methods-use-this */
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Booking = () => <h2>Booking</h2>;
+const Contact = () => <h2>Contact</h2>;
+const Header = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/about">About</Link>
+    </li>
+    <li>
+      <Link to="/booking">Booking</Link>
+    </li>
+  </ul>
+);
 
-export default App;
+const AppRouter = () => (
+  <Router>
+    <div>
+      <Header />
+
+      <Route path="/" exact component={Index} />
+      <Route path="/about/" component={About} />
+      <Route path="/booking/" component={Booking} />
+      <Route path="/contact/" component={Contact} />
+    </div>
+  </Router>
+);
+
+export default AppRouter;
